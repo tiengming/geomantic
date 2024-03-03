2966528efdf60504f279ed00870aaa39bca37ed6
沐竹雨 <39575199+muzhuyu@users.noreply.github.com>
2024-03-03 22:36:49 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -15,9 +15,9 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '学习生活',
+  HEO_HERO_TITLE_1: '记录学习生活',
   HEO_HERO_TITLE_2: '感知知识的魅力',
-  HEO_HERO_TITLE_3: 'BLOG.MUZHUYU.COM',
+  HEO_HERO_TITLE_3: 'MUZHUYU.CN',
   HEO_HERO_TITLE_4: '持续学习',
   HEO_HERO_TITLE_5: '重点学习内容',
   HEO_HERO_TITLE_LINK: 'https://muzhuyu.cn',
