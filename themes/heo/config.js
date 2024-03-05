b685eb0fe2dda13a0848ce771d4fb41bd7636f20
沐竹雨 <39575199+muzhuyu@users.noreply.github.com>
2024-03-05 21:55:34 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -23,7 +23,7 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: 'https://muzhuyu.cn',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '学习重点', url: '/tag/学习重点' },
+  HEO_HERO_CATEGORY_1: { title: '程序代码', url: '/tag/程序代码' },
   HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
   HEO_HERO_CATEGORY_3: { title: '学习记录', url: '/tag/学习记录' },
 
